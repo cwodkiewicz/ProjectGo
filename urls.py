96a32e995d1bf7bd8c45f74f7f6893dae0a2df0d
cwodkiewicz <cwodkiewicz@gmail.com>
2022-11-24 03:31:30 -0500

    deleted files

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .go.users import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout")
-]
