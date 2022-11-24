604f2a4783b374bd22f68bbe285dbdfa0fdc94bf
cwodkiewicz <cwodkiewicz@gmail.com>
2022-11-23 20:26:22 -0500

    added login and registration

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .go.users import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
+]
